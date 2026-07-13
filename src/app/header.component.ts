75a546eec2c9bcd54af2af6fbd28abe145cdbf8d
KoiOriginal <photsathon.sangsukwaw@gmail.com>
2026-07-13 23:08:26 +0200

    ีupdate tsconfig.app.json

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    template: `<h1>Header</h1>`,
+    standalone: true,
+    templateUrl: `./header.component.html`
 })
 export class HeaderComponent {}
