3f33d92b7b45860602f05130d247c03b3c949b56
KoiOriginal <photsathon.sangsukwaw@gmail.com>
2026-07-18 19:07:52 +0200

    Add dummy user data and display random user

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,17 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Monkey D. Luffy',
+    avatar: 'user-1.jpg'
+  },
+  {
+    id: 'u2',
+    name: 'Tony Tony Chopper',
+    avatar: 'user-2.jpg'
+  },
+  {
+    id: 'u3',
+    name: 'Brook',
+    avatar: 'user-3.jpg'
+  }
+];
