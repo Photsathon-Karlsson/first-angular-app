43fabed8019d8d6250804a752f5d4a27eaa60098
KoiOriginal <photsathon.sangsukwaw@gmail.com>
2026-07-27 15:12:14 +0200

    Use a typed user object as component input

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,12 @@
 import { Component, input, output } from '@angular/core'; // Imports Component, input, and output from Angular
 
+// Defines the structure of a user object
+type UserData = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
 @Component({
   selector: 'app-user',         // The HTML tag used to show this component
   imports: [],                  // No other components are used here
@@ -8,25 +15,19 @@ import { Component, input, output } from '@angular/core'; // Imports Component, 
 })
 
 export class User {
-  // Receives the required user ID from the parent component
-  id = input.required<string>();
-
-  // Receives the required user name from the parent component
-  name = input.required<string>();
-
-  // Receives the required avatar filename from the parent component
-  avatar = input.required<string>();
+  // Receives the required user object from the parent component
+  user = input.required<UserData>();
 
   // Creates a custom event that sends the user ID to the parent component
   select = output<string>();
 
   // Sends the selected user's ID to the parent component
   onSelectUser() {
-    this.select.emit(this.id());
+    this.select.emit(this.user().id);
   }
 
   // Creates and returns the user's image path
   get imagePath() {
-    return 'users/' + this.avatar();
+    return 'users/' + this.user().avatar;
   }
 }
