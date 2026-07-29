e45874527fee661b177b131a8abc2d429a13ccb9
KoiOriginal <photsathon.sangsukwaw@gmail.com>
2026-07-29 20:21:38 +0200

    Use @for and @if to display users and conditional content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,28 +1,32 @@
 import { Component, signal } from '@angular/core'; // Imports Component and signal from Angular
-import { HeaderComponent } from './header/header.component'; // Imports the header component from the header folder
+import { HeaderComponent } from './header/header.component'; // Imports the header component
 import { User } from './user/user'; // Imports the user component
 import { ActiveUser } from './active-user/active-user'; // Imports the active user component
 import { DUMMY_USERS } from './dummy-users'; // Imports the user data
 
 @Component({
-  selector: 'app-root',                                // The main application component
-  imports: [HeaderComponent, User, ActiveUser],        // Makes the components available
-  templateUrl: './app.html',                           // Connects this component to its HTML file
-  styleUrl: './app.css'                                // Connects this component to its CSS file
+  selector: 'app-root',                         // The main application component
+  imports: [HeaderComponent, User, ActiveUser], // Makes the components available
+  templateUrl: './app.html',                    // Connects this component to its HTML file
+  styleUrl: './app.css'                         // Connects this component to its CSS file
 })
 
 export class App {
-  // Stores the currently selected user inside a Signal
-  selectedUser = signal(DUMMY_USERS[0]);
+  // Makes all users available in the template
+  users = DUMMY_USERS;
 
-  // Receives the selected user ID and selects a new random user
+  // Stores the selected user ID or undefined before a user is selected
+  selectedUserId = signal<string | undefined>(undefined);
+
+  // Finds and returns the selected user
+  get selectedUser() {
+    return this.users.find(
+      (user) => user.id === this.selectedUserId()
+    );
+  }
+
+  // Receives and stores the selected user ID
   onSelectUser(id: string) {
-    // Shows the user ID received from the child component
-    console.log('Selected user ID:', id);
-
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
-    // Updates the Signal with a new user
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.selectedUserId.set(id);
   }
 }
